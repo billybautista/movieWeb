14dbd72a4c94388916080e9a24f12757b94c8057
billybautista <billy.bautista.cornelio@gmail.com>
2021-10-20 21:21:10 -0300

    added .env

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,21 +1,19 @@
 import axios from "../modules/axios";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(process.env);
 export const getDiscoveryMovie = () => {
   return axios
-    .get("/discover/movie?api_key=f489c6a91feead934e32a34d0011a8f6&page=1")
+    .get(`/discover/movie?api_key=${API_KEY}`)
     .then((res) => res.data);
 };
 
 export const getMovieDetail = (id) => {
-  return axios
-    .get(`/movie/${id}?api_key=f489c6a91feead934e32a34d0011a8f6`)
-    .then((res) => res.data);
+  return axios.get(`/movie/${id}?api_key=${API_KEY}`).then((res) => res.data);
 };
 
 export const getMovieRecomendations = (id) => {
   return axios
-    .get(
-      `/movie/${id}/recommendations?api_key=f489c6a91feead934e32a34d0011a8f6`
-    )
+    .get(`/movie/${id}/recommendations?api_key=${API_KEY}`)
     .then((res) => res.data);
 };
